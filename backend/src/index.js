eef4506a6fca11e2f620f9d30bc2181bfd2edab9
Cristhyan Kohlhase <cristhyan.ko@outlook.com>
2019-02-06 18:59:12 -0300

    Real Time with socket.io

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,25 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const routes = require
+const cors = require('cors')
 
 const port = 3000
 const app = express()
+
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
 
 mongoose.connect('mongodb://localhost:27017/goweek-backend', {
     useNewUrlParser: true
 })
 
+app.use((req, res, next) => {
+    req.io = io
+    return next()
+})
+app.use(cors)
+app.use(express.json())
 app.use(require('./routes'))
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server started on port ${port}`)
 })
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const TweetController = require('./constrollers/TweetController')
+const LikeController = require('./constrollers/LikeController')
 
 const routes = express.Router()
 
-routes.get('/', (req, res) => {
-    res.send('Hello Word')
-})
+routes.get('/tweets', TweetController.index)
+routes.post('/tweets', TweetController.store)
+routes.post('/like/:id', LikeController.store)
 
 module.exports = routes
