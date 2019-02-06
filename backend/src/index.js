ed3aac87ae48b75af624d5b1b13756a08efc512a
Cristhyan <cristhyan.ko@outlook.com>
2019-02-06 17:03:57 -0300

    Start config app with mongodb and MVC

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,15 @@
 const express = require('express')
-const app = express()
+const mongoose = require('mongoose')
+const routes = require
 
 const port = 3000
+const app = express()
 
-app.get('/', (req, res) => {
-    res.send('Hello Word')
+mongoose.connect('mongodb://localhost:27017/goweek-backend', {
+    useNewUrlParser: true
 })
+
+app.use(require('./routes'))
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`)
